0583f84a6d3b3482a982c45025e94d3128888712
marcsello <punkosdmarcell@rocketmail.com>
2023-10-29 20:38:52 +0100

    implemented flag logic

--- gamemode/sh_flag.lua
+++ gamemode/sh_flag.lua
@@ -15,6 +15,9 @@ local FLAG_EVENT_CAPTURE = 3
 local NET_EVENT_LEN = 2 -- in bits
 local NET_FLAG_ID_LEN = 4 -- More than 15 teams will break this
 
+local FLAG_RETURN_TIME = 30 -- TODO: make it a convar
+local FLAG_GRAB_DISTANCE = 2300
+
 FlagManager = FlagManager or { -- prevent re-creating the whole object on
     flags = {}
 }
@@ -73,8 +76,10 @@ function FlagManager:DropFlag(flagID, droppedPos, droppedTs)
     self.flags[flagID].droppedPos = droppedPos
     self.flags[flagID].droppedTs = droppedTs
 
-    local teamName = team.GetName(self.flags[flagID].belongsToTeam)
+    local teamName = team.GetName(flagID)
     print("The flag of " .. teamName .. " has been dropped")
+
+    hook.Run("B2CTF_FlagDropped", flagID, droppedPos, droppedTs, droppedTs + FLAG_RETURN_TIME) -- teamID, dropPos, dropTime, autoReturnTime
 
     -- Sync
     if SERVER then
@@ -87,26 +92,38 @@ function FlagManager:DropFlag(flagID, droppedPos, droppedTs)
     end
 end
 
-function FlagManager:ReturnFlag(flagID)
+function FlagManager:ReturnFlag(flagID, ply)
     assert(self:FlagIDValid(flagID), "invalid flag id")
 
     self.flags[flagID].grabbedBy = nil
     self.flags[flagID].droppedPos = nil
     self.flags[flagID].droppedTs = nil
 
-    local teamName = team.GetName(self.flags[flagID].belongsToTeam)
-    print("The flag of " .. teamName .. " has been returned")
+    local autoReturn = ply == nil -- no player = auto-return
+
+    local teamName = team.GetName(flagID) -- flagID == teamID
+    if autoReturn then
+        print("The flag of " .. teamName .. " has been returned (auto return)")
+    else
+        print("The flag of " .. teamName .. " has been returned by " .. ply:Nick())
+    end
+
+    hook.Run("B2CTF_FlagReturned", flagID, self.flags[flagID].homePos, autoReturn, ply) -- teamID, homePos, autoReturn, returner
 
     -- Sync
     if SERVER then
         net.Start("B2CTF_FlagEventUpdate")
         net.WriteUInt(FLAG_EVENT_RETURN, NET_EVENT_LEN)
         net.WriteUInt(flagID, NET_FLAG_ID_LEN)
+        net.WriteBool(autoReturn)
+        if not autoReturn then
+            net.WriteEntity(ply)
+        end
         net.Broadcast()
     end
 end
 
-function FlagManager:GrabFlag(flagID, ply)
+function FlagManager:GrabFlag(flagID, ply, flagWasDropped)
     assert(self:FlagIDValid(flagID), "invalid flag id")
     if not (ply and IsValid(ply)) then return end
 
@@ -114,8 +131,14 @@ function FlagManager:GrabFlag(flagID, ply)
     self.flags[flagID].droppedPos = nil
     self.flags[flagID].droppedTs = nil
 
-    local teamName = team.GetName(self.flags[flagID].belongsToTeam)
-    print("The flag of " .. teamName .. " is grabbed by " .. ply:Nick())
+    local teamName = team.GetName(flagID)
+    if flagWasDropped then
+        print("The flag of " .. teamName .. " is grabbed by " .. ply:Nick() .. " (from ground)")
+    else
+        print("The flag of " .. teamName .. " is grabbed by " .. ply:Nick() .. " (from base)")
+    end
+
+    hook.Run("B2CTF_FlagGrabbed", flagID, ply, flagWasDropped) -- teamID, player
 
     -- Sync
     if SERVER then
@@ -123,17 +146,26 @@ function FlagManager:GrabFlag(flagID, ply)
         net.WriteUInt(FLAG_EVENT_GRAB, NET_EVENT_LEN)
         net.WriteUInt(flagID, NET_FLAG_ID_LEN)
         net.WriteEntity(ply)
+        net.WriteBool(flagWasDropped)
         net.Broadcast()
     end
 end
 
-function FlagManager:CaptureFlag(flagID, capturedBy)
+function FlagManager:CaptureFlag(flagID, capturedBy) -- flagID is the ID of the flag that is captured by capturedBy
     assert(self:FlagIDValid(flagID), "invalid flag id")
     if not (capturedBy and IsValid(capturedBy)) then return end
 
     self.flags[flagID].grabbedBy = nil
     self.flags[flagID].droppedPos = nil
     self.flags[flagID].droppedTs = nil
+
+    local teamName = team.GetName(flagID)
+    local capturerTeamID = capturedBy:Team()
+    local capturerTeamName = team.GetName(capturerTeamID)
+    print("The flag of " .. teamName .. " is was captured by " .. capturedBy:Nick() .. " (score to team " .. capturerTeamName .. ")")
+    team.AddScore(capturerTeamID, 1)
+
+    hook.Run("B2CTF_FlagCaptured", flagID, capturerTeamID, capturedBy) -- flagID / flag's teamID, capturing team's teamID, capturer
 
     -- Sync
     if SERVER then
@@ -190,23 +222,60 @@ if SERVER then
 
     function FlagManager:_playerThink(ply)
         -- The main business logic
-        local potentialCapture = nil
-        for i, v in ipairs(self.flags) do -- Normally I would write two for loops, but this feels a little more optimized
+        local interactionFlagID = nil
+        local playerCarryingFlagID = nil
+        local interactionFlagDropped = false
+        for i, flag in self:IterFlags() do -- Normally I would write two for loops, but this feels a little more optimized
 
-            if v.grabbedBy == ply then return end -- the player is already holding a flag
+            if flag.grabbedBy == ply then
+                 -- this player is carrying this flag. (so the player is carrying a flag)
+                playerCarryingFlagID = i
+                continue -- don't process this flag further (it's position is irrelevant)
+            end
 
-            if v.grabbedBy then continue end -- ignore grabbed flags
-            if v.belongsToTeam == ply:Team() then continue end -- ignore own flags
-            if ply:GetPos():DistToSqr(v.homePos) < 2100 then
-                potentialCapture = i
+            if flag.grabbedBy then continue end -- this flag is grabbed by someone, ignore it
+
+            local checkPos = flag.homePos
+            local dropped = false
+            if flag.droppedPos then
+                local dropTime = CurTime() - flag.droppedTs
+                if dropTime < FLAG_RETURN_TIME then  -- ignore flag if it's auto return time already spent, to avoid undefined states
+                    checkPos = flag.droppedPos
+                    dropped = true
+                end
+            end
+
+            if ply:GetPos():DistToSqr(checkPos) < FLAG_GRAB_DISTANCE then
+                interactionFlagID = i
+                interactionFlagDropped = dropped
+            end
+
+        end
+
+        if interactionFlagID then -- the player is interacting with a flag
+
+            if (not playerCarryingFlagID) and (interactionFlagID != ply:Team()) then
+                -- the player not carrying flag, and this is not own flag... possibly taking a flag
+                self:GrabFlag(interactionFlagID, ply, interactionFlagDropped)
+                return -- only handle one action per tick, so that handles can correctly process
+            end
+
+            if (interactionFlagID == ply:Team()) then
+                if interactionFlagDropped then
+                    -- this is their own flag, and it is dropped... return it
+                    self:ReturnFlag(interactionFlagID, ply)
+                    return -- only handle one action per tick, so that handles can correctly process
+                elseif playerCarryingFlagID then
+                    -- the flag is their own, not dropped and the player is carrying a flag... this is a capture
+                    self:CaptureFlag(playerCarryingFlagID, ply)
+                    return -- only handle one action per tick, so that handles can correctly process
+                end
             end
         end
-        if potentialCapture then
-            self:GrabFlag(potentialCapture, ply)
-        end
+
     end
 
-    function FlagManager:_checkAndDropFlag(ply)
+    function FlagManager:_checkAndDropFlagOnDeath(ply)
         local flagID = self:GetFlagIDGrabbedByPlayer(ply)
         if not flagID then return end -- player wasn't holding any of the flags
         -- Run a trace bellow the player to find the ground, so flags won't be dropped mid-air
@@ -223,6 +292,17 @@ if SERVER then
         self:DropFlag(flagID, dropPos, CurTime())
     end
 
+    function FlagManager:_svThink()
+        for i, flag in self:IterFlags() do
+            if flag.droppedTs and flag.droppedPos and (not flag.grabbedBy) then
+                local dropTime = CurTime() - flag.droppedTs
+                if dropTime >= FLAG_RETURN_TIME then
+                    self:ReturnFlag(i, nil) -- nil ply = autoReturn
+                end
+            end
+        end
+    end
+
     -- then call them when appropriate
 
     hook.Add("PlayerTick", "B2CTF_FlagManagerPlayerThink", function(ply) -- WARNING: Does not run when in vehicle! (but that's fine)
@@ -232,12 +312,17 @@ if SERVER then
         end
     end)
 
-    hook.Add( "PlayerDeath", "B2CTF_DropFlag", function( victim, inflictor, attacker )
+    hook.Add("PlayerDeath", "B2CTF_DropFlag", function( victim, inflictor, attacker )
         if not Phaser:CurrentPhaseInfo().fightAllowed then return end -- do nothing, when we are not fighting
         if IsValid(victim) and victim:IsPlayer() and victim:TeamValid() then
-            FlagManager:_checkAndDropFlag(victim)
+            FlagManager:_checkAndDropFlagOnDeath(victim)
         end
     end )
+
+    hook.Add("Think", "B2CTF_FlagThink", function()
+        if not Phaser:CurrentPhaseInfo().fightAllowed then return end -- do nothing, when we are not fighting
+        FlagManager:_svThink()
+     end)
 
     hook.Add("PlayerInitialSpawn", "B2CTF_SendInitialFlagsState", function(ply) FlagManager:_sendFullSyncToPlayer(ply) end )
 
@@ -259,7 +344,7 @@ end)
 
 
 if CLIENT then -- sync recv
-    net.Receive( "B2CTF_FlagEventUpdate", function( len, ply )
+    net.Receive( "B2CTF_FlagEventUpdate", function( len, ply ) -- sync client
         if ( IsValid( ply ) and ply:IsPlayer() ) then return end -- disallow these messages from players
 
         local event = net.ReadUInt(NET_EVENT_LEN)
@@ -273,13 +358,19 @@ if CLIENT then -- sync recv
 
         if event == FLAG_EVENT_RETURN then
             local flagID = net.ReadUInt(NET_FLAG_ID_LEN)
-            FlagManager:ReturnFlag(flagID)
+            local autoReturn = net.ReadBool()
+            local returner = nil
+            if not autoReturn then
+                returner = net.ReadEntity()
+            end
+            FlagManager:ReturnFlag(flagID, returner)
         end
 
         if event == FLAG_EVENT_GRAB then
             local flagID = net.ReadUInt(NET_FLAG_ID_LEN)
             local grabber = net.ReadEntity()
-            FlagManager:GrabFlag(flagID, grabber)
+            local flagWasDropped = net.ReadBool()
+            FlagManager:GrabFlag(flagID, grabber, flagWasDropped)
         end
 
         if event == FLAG_EVENT_CAPTURE then
